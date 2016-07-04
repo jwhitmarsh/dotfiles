d59633ef0339ad33d685f25135a529862fa2620c
James Whitmarsh <jwhitmarsh@dresources.com>
2016-07-04 14:55:21 +0100

    update setup.sh to check if files exist and prompt to run with force if required

--- setup.sh
+++ setup.sh
@@ -2,9 +2,35 @@
 
 # need to document
 HERE=$(pwd)
-echo 'Setup dotfiles'
+echo 'Setting up dotfiles...'
 
-ln -s $HERE/bashrc ~/.bashrc
-ln -s $HERE/aliases ~/.bash_aliases
-ln -s $HERE/profile ~/.bash_profile
-ln -s $HERE/liquidpromptrc ~/.liquidpromptrc
+BASHRC=~/.bashrc
+ALIASES=~/.bash_aliases
+PROFILE=~/.bash_profile
+LIQUIDPROMPT=~/.liquidpromptrc
+GITCONFIG=~/.gitconfig
+GITMESSAGE=~/.gitmessage
+GITIGNOREGLOBAL=~/.gitignore_global
+WITHFORCE=''
+
+if [[ $* == *--force* ]]; then
+  WITHFORCE='-f'
+fi
+
+if [ -f "$BASHRC" ] || [ -f "$ALIASES" ] || [ -f "$PROFILE" ] || [ -f "$LIQUIDPROMPT" ] || [ -f "$GITCONFIG" ] ||
+[ -f "$GITMESSAGE" ] || [ -f "$GITIGNOREGLOBAL" ] && [[ $* != *--force* ]]
+then
+  echo "Symlinked file(s) already exists"
+  echo "Run this script with --force if you are feeling brave.."
+  exit -1
+fi
+
+ln -s $WITHFORCE $HERE/bashrc $BASHRC
+ln -s $WITHFORCE $HERE/aliases $ALIASES
+ln -s $WITHFORCE $HERE/profile $PROFILE
+ln -s $WITHFORCE $HERE/liquidpromptrc $LIQUIDPROMPT
+ln -s $WITHFORCE $HERE/gitconfig $GITCONFIG
+ln -s $WITHFORCE $HERE/gitmessage $GITMESSAGE
+ln -s $WITHFORCE $HERE/gitignore_global $GITIGNOREGLOBAL
+ln -s $WITHFORCE $HERE/gitconfig ~/nowhere
+echo 'Done'
